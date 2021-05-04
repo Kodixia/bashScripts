3aa7235dcbd31e25129e5523d62bf208689cdf4d
dafty <julioTajonar@hotmail.com>
2021-05-04 06:01:18 -0500

    Adding color to the script and implementing the correct name of the keys

--- setsshgit.sh
+++ setsshgit.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-if [[ -f $HOME/.ssh/keys/gitkey ]]
-then
-	echo "File located.."
-	eval $(ssh-agent -s)
-	ssh-add $HOME/.ssh/keys/gitkey
+greenColor="\x1B[0;32m\033[1m"
+redColor="\x1B[0;31m\033[1m"
+blueColor="\x1B[0;34m\033[1m"
+yellowColor="\x1B[0;33m\033[1m"
+purpleColor="\x1B[0;35m\033[1m"
+turquoiseColor="\x1B[0;36m\033[1m"
+grayColor="\x1B[0;37m\033[1m"
+endColor="\033[0m\x1B[0m"
+#"${Color} ${endColor}"
+#"${} ${enColor}"
+
+if [[ -f $HOME/.ssh/keys/gitKey ]];then
+	echo -e "${greenColor} File located.. ${endColor} "
+	eval $(ssh-agent -s) 
+	ssh-add $HOME/.ssh/keys/gitKey
 	ssh -T git@github.com
       	bash	
 else
